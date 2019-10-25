904ed596bd067d8c6e9727ad0c4cc7565fb48a7c
Bionda98 <56724049+Bionda98@users.noreply.github.com>
2019-10-25 17:26:41 +0200

    /build

---
+++ CMakeFiles/TestNeuron.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestNeuron.dir/src/test_main.cpp.o"
+  "CMakeFiles/TestNeuron.dir/src/neuron.cpp.o"
+  "CMakeFiles/TestNeuron.dir/src/network.cpp.o"
+  "CMakeFiles/TestNeuron.dir/src/simulation.cpp.o"
+  "CMakeFiles/TestNeuron.dir/src/random.cpp.o"
+  "TestNeuron.pdb"
+  "TestNeuron"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestNeuron.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
