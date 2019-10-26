743b21a00bbba94da72cd62b11c7bf82db1e8c46
Bionda98 <56724049+Bionda98@users.noreply.github.com>
2019-10-26 14:30:51 +0200

    Delete CTestTestfile.cmake

--- CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bionda/myfiles/Programmation/NeuronNet-1
-# Build directory: /home/bionda/myfiles/Programmation/NeuronNet-1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(NeuronNet "TestNeuron")
